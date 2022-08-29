7e25fdcc56e01098baf87a21db473832023a858d
pedersor <pedersor@uci.edu>
2022-08-29 13:01:00 -0700

    add setup and readme

---
+++ readme.md
@@ -0,0 +1,22 @@
+Conditional Probability Density Functional Theory (CP-DFT) with 1D systems
+===============================
+
+Installation (Linux)
+------------
+
+* First install the [DFT_1D](https://github.com/pedersor/DFT_1d) module and dependencies:
+```
+$ git clone https://github.com/pedersor/DFT_1d.git
+$ cd DFT_1d
+$ pip install .
+```
+
+* Next, install the conditional probability DFT module 
+```
+$ git clone https://github.com/pedersor/cond_prob_dft_1d.git
+$ cd cond_prob_dft_1d
+$ pip install .
+```
+
+## Disclaimer
+This library is in *alpha*.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+    name="cond_prob_dft_1d",
+    version="0.0.0",
+    author="Ryan Pederson",
+    author_email="pedersor@uci.edu",
+    description="Conditional probability DFT in 1D examples.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pedersor/cond_prob_dft_1d",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=('>=3.6.9'),
+    packages=find_packages(),
+)
